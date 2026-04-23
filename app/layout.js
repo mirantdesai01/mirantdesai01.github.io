402ccd7338ed527f119231c8ec3201caec5f6602
Mirant <mirantdesai@icloud.com>
2026-04-23 09:44:37 -0500

    Use correct Google meta verification token

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ export const metadata = {
   title: "Mirant Desai",
   description: "Personal site for Mirant Desai",
   verification: {
-    google: "google-site-verification=J7i7zlBRp000_K6YT3p1HkKYJhp7qw6CGpBPfghCf3I",
+    google: "wu2Rg7sElRUMCJdfsQN8aoiVRF2HtPFrtXLdUHJL2hM",
   },
 };
 
