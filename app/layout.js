963da3837ab071d0a0c91c16a5758db87b0f2ced
Mirant <mirantdesai@icloud.com>
2026-04-23 09:33:43 -0500

    Add Google Search Console verification

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Mirant Desai",
   description: "Personal site for Mirant Desai",
+  verification: {
+    google: "google-site-verification=J7i7zlBRp000_K6YT3p1HkKYJhp7qw6CGpBPfghCf3I",
+  },
 };
 
 export default function RootLayout({ children }) {
